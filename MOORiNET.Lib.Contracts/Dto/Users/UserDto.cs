ce4b5416639c8aa1773209bf9d6e93efc39c698c
Александр <@DESKTOP-L9V170T>
2025-05-08 14:20:39 +0300

    Обновление моделей

---
+++ MOORiNET.Lib.Contracts/Dto/Users/UserDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MOORiNET.Lib.Enums;
+
+#nullable enable
+namespace MOORiNET.Lib.Contracts.Dto
+{
+    /// <summary>
+    /// Модель пользователя
+    /// </summary>
+    public class UserDto
+    {
+        /// <summary>
+        /// ИД
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// ФИО
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// E-mail
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Статус
+        /// </summary>
+        public UserStatusEnum Staus { get; set; }
+    }
+}
---
+++ MOORiNET.Lib.Contracts/Dto/Users/UserLoginRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace MOORiNET.Lib.Contracts.Dto
+{
+    /// <summary>
+    /// Модель авторизации пользователя
+    /// </summary>
+    public class UserLoginRequest
+    {
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Username { get; set; } = null!;
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string Password { get; set; } = null!;
+    }
+}
