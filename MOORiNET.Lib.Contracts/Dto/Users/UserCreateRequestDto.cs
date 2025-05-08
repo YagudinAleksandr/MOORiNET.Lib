d997256ea1f1f6ec136c34f96d929bc82c329b6a
Александр <@DESKTOP-L9V170T>
2025-05-08 13:32:51 +0300

    Добавьте файлы проекта.

---
+++ MOORiNET.Lib.Contracts/Dto/Users/UserCreateRequestDto.cs
@@ -0,0 +1,35 @@
+﻿using MOORiNET.Lib.Enums;
+
+namespace MOORiNET.Lib.Contracts.Dto.Users
+{
+    /// <summary>
+    /// Модель создания пользователя
+    /// </summary>
+    public class UserCreateRequestDto
+    {
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Username { get; set; } = null!;
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string Password { get; set; } = null!;
+
+        /// <summary>
+        /// E-mail
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// ФИО
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Статус активности <see cref="UserStatusEnum"/>
+        /// </summary>
+        public UserStatusEnum Status { get; set; }
+    }
+}
---
+++ MOORiNET.Lib.Enums/UserStatusEnum.cs
@@ -0,0 +1,23 @@
+﻿namespace MOORiNET.Lib.Enums
+{
+    /// <summary>
+    /// Статусы пользователя
+    /// </summary>
+    public enum UserStatusEnum
+    {
+        /// <summary>
+        /// Не активен
+        /// </summary>
+        NotActive = 0,
+
+        /// <summary>
+        /// Активен
+        /// </summary>
+        Active = 1,
+
+        /// <summary>
+        /// Заблокирован
+        /// </summary>
+        Blocked = 2
+    }
+}
