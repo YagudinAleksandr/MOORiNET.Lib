4dbf7648e7470c914f86071cfb245f867605e70a
Александр <@DESKTOP-L9V170T>
2025-05-08 22:26:45 +0300

    Модель запроса к API

---
+++ MOORiNET.Lib.Contracts/Dto/ApiRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace MOORiNET.Lib.Contracts.Dto
+{
+    /// <summary>
+    /// Запрос к API
+    /// </summary>
+    /// <typeparam name="TResponse">Тип ответа</typeparam>
+    public abstract class ApiRequestDto<TResponse> : IApiRequestDto where TResponse : class
+    {
+        public abstract string Endpoint { get; }
+
+        public virtual HttpMethod Method => HttpMethod.Get;
+        public virtual Dictionary<string, string> Headers => new Dictionary<string, string>();
+        
+        /// <summary>
+        /// Тело запроса опционально для POST/PUT
+        /// </summary>
+        public virtual object Body => null;
+    }
+}
---
+++ MOORiNET.Lib.Contracts/Dto/IApiRequestDto.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace MOORiNET.Lib.Contracts.Dto
+{
+    /// <summary>
+    /// Базовая модель запроса
+    /// </summary>
+    public interface IApiRequestDto
+    {
+        /// <summary>
+        /// Точка входа в API
+        /// </summary>
+        string Endpoint { get; }
+
+        /// <summary>
+        /// Тип запроса
+        /// </summary>
+        HttpMethod Method { get; }
+
+        /// <summary>
+        /// Заголовки
+        /// </summary>
+        Dictionary<string, string> Headers { get; }
+    }
+}
