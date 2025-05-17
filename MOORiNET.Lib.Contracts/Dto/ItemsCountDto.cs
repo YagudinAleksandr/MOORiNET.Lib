2aa4e4d71611348c3aba7b5d61823032b2f74173
Александр <@DESKTOP-L9V170T>
2025-05-17 22:00:09 +0300

    DTO для вывода количественных данных

---
+++ MOORiNET.Lib.Contracts/Dto/ItemsCountDto.cs
@@ -0,0 +1,30 @@
+﻿namespace MOORiNET.Lib.Contracts.Dto
+{
+    /// <summary>
+    /// Модель с количеством объектов
+    /// </summary>
+    /// <typeparam name="T">Тип данных</typeparam>
+    public class ItemsCountDto<T>
+    {
+        /// <summary>
+        /// Количество данных
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Массив данных
+        /// </summary>
+        public T[] Data { get; private set; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="count">Количество данных</param>
+        /// <param name="data">Массив данных</param>
+        public ItemsCountDto(int count, T[] data) 
+        {
+            Count = count;
+            Data = data;
+        }
+    }
+}
